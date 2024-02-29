43d3aa9b148b49e4864dd5fa44d7a2280f0e78d8
Amanda Conde <flab.amanda@gmail.com>
2024-02-29 14:18:26 -0300

    Verificador de vizinhos que estão vivos

---
+++ GameOfLife/Verificacao.swift
@@ -0,0 +1,24 @@
+//
+//  Verificacao.swift
+//  GameOfLife
+//
+//  Created by Amanda Conde on 28/02/24.
+//
+
+import SwiftUI
+
+struct Verificacao: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Verificacao()
+}
+
+//variavel que retorna um inteiro que fala quantos vizinhos tem
+//encontrar o elemento, mas o estado dele ainda nao importa
+//switch case com as possibilidades
+//verificar se o board existe: 
+
