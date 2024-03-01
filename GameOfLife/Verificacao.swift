a8bbdf98fa674511b1b02ce3f41df1cd9f941c54
Amanda Conde <flab.amanda@gmail.com>
2024-03-01 15:11:02 -0300

    FINAL

--- GameOfLife/Verificacao.swift
+++ GameOfLife/Verificacao.swift
@@ -7,35 +7,38 @@
 
 import SwiftUI
 //função vai verificar os vizinhos vivos
-extension ContentView{
+extension ContentView {
     
-    func verificacao (i: Int, j: Int, matriz:[[Int]]) -> Int{
+    func verificacao(i: Int, j: Int, matriz: [[Int]]) -> Int {
         var qtdVivo = 0
-        if matriz.count-1 != 0 && matriz[0].count-1 != 0{
-            for minL in max(0, i - 1)...min(matriz.count - 1, i + 1){
-                for minC in max(0, j - 1 )...min(matriz [0].count-1, j + 1){
-                    if !(i == minL || j == minC) {
-                        if matriz[minL][minC] == 1{
-                            qtdVivo += 1
-                        }
-                    }
+    
+        if matriz.count - 1 != 0 && matriz[0].count - 1 != 0 {
+          for minL in max(0, i - 1)...min(matriz.count - 1, i + 1) {
+            for minC in max(0, j - 1)...min(matriz[0].count - 1, j + 1){
+              if !(i == minL && j == minC) {
+                if matriz [minL][minC] == 1 {
+                  qtdVivo += 1
                 }
+              }
             }
+          }
         }
         return qtdVivo
-    }
+      }
     
-    func regrasGeracao(){
-        var matrizCopia = Array(repeating: Array(repeating: 0, count: 10), count:10 )
+    
+    func regrasGeracao() {
+        var matrizCopia = self.matriz
         
-        //
+        
+
         for i in 0...matriz.count - 1{
             for j in 0...matriz.count - 1{
-                var vizinhos = verificacao(i: i, j: j, matriz: matriz)
+                let vizinhos = verificacao(i: i, j: j, matriz: matriz)
                 
                 //se estiver vivo e for menor que 2 vizinhos: morre OU se vivo e for maior que 3 vizinhos: morre
                 if vizinhos < 2 || vizinhos > 3 {
-                    matrizCopia[i][j]
+                    matrizCopia[i][j] = 0
                 }
                 
                 
@@ -55,3 +58,4 @@ extension ContentView{
     }
     
 }
+
